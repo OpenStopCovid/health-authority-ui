1bc98aa7fd1883fa00bc231978cb0cba596346f3
Mathieu Agopian <mathieu@agopian.info>
2020-04-15 21:24:01 +0200

    Ask for a new code

--- index.js
+++ index.js
@@ -4,11 +4,32 @@ const loginURL = "./?auth_token=123456";
 
 let userToken = undefined;
 
+/* EVENT LISTENERS */
 document.addEventListener("click", (event) => {
   if (event.target.getAttribute("data-behavior") !== "login") return;
   initiateAuth();
+  event.preventDefault();
 });
 
+document.addEventListener("click", (event) => {
+  if (event.target.getAttribute("data-behavior") !== "new-code") return;
+  refreshCode();
+  event.preventDefault();
+});
+
+document.addEventListener("code-refreshed", (event) => {
+  console.log("received code-refreshed event", event);
+  if (!event.detail || !event.detail.pincode || !event.detail.qrcode) return;
+  const { pincode, qrcode } = event.detail;
+  const pincodeEl = document.querySelector("#pin-code");
+  const qrcodeEl = document.querySelector("#qr-code");
+  if (!pincodeEl || !qrcodeEl) return;
+  pincodeEl.textContent = pincode;
+  qrcodeEl.src = qrcode;
+  console.log("refreshed the codes");
+});
+
+/* AUTH */
 const initiateAuth = () => {
   // Redirect to the login URL
   document.location = loginURL;
@@ -28,10 +49,23 @@ const checkLoggedIn = () => {
 
 const setLoggedIn = (userToken) => {
   // Display the "logged in" screen
-  document.body.className = "logged-in";
+  document.querySelector("#wrapper").className = "logged-in";
   // Remove the auth-token from the url
   history.replaceState(null, "", "./");
 };
 
-// MAIN
+/* VALIDATION CODES */
+
+const refreshCode = () => {
+  const event = new CustomEvent("code-refreshed", {
+    detail: {
+      pincode: 654321,
+      qrcode: "QR2.png",
+    },
+  });
+  console.log("firing code-refreshed event");
+  document.dispatchEvent(event);
+};
+
+/* MAIN */
 checkLoggedIn();
