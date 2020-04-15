861e9d3f6a39f6c180e24351a47a0bf0d92f503a
Mathieu Agopian <mathieu@agopian.info>
2020-04-15 11:34:48 +0200

    logged-in/logged-out display

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+// TODO: use an env variable?
+// const loginURL = "/login";
+const loginURL = "./?auth_token=123456";
+
+let userToken = undefined;
+
 document.addEventListener("click", (event) => {
   if (event.target.getAttribute("data-behavior") !== "login") return;
-  console.log("login clicked");
+  initiateAuth();
 });
+
+const initiateAuth = () => {
+  document.location = loginURL;
+};
+
+const checkLoggedIn = () => {
+  const searchParams = new URLSearchParams(
+    document.location.search.replace(/\?/, "")
+  );
+  userToken = searchParams.get("auth_token");
+  if (userToken) {
+    setLoggedIn();
+  }
+};
+
+const setLoggedIn = () => {
+  document.body.className = "logged-in";
+};
+
+// MAIN
+checkLoggedIn();
