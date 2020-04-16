acbdcb1cfeb0205e9b3da19795f73e3442dd498c
Mathieu Agopian <mathieu@agopian.info>
2020-04-16 17:51:36 +0200

    Use a list of handlers for the click events

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 /* EVENT LISTENERS */
 document.addEventListener("click", (event) => {
-  if (event.target.getAttribute("data-behavior") !== "generate-code") return;
-  generateCode();
-  displayCode();
+  const handlers = {
+    "generate-code": generateCode,
+    "print-code": printCode,
+    "email-code": emailCode,
+    "display-pincode": displayPinCode,
+  };
+  const behavior = event.target.getAttribute("data-behavior");
+  const handler = handlers[behavior];
+  if (!handler) return;
   event.preventDefault();
-});
-
-document.addEventListener("click", (event) => {
-  if (event.target.getAttribute("data-behavior") !== "new-code") return;
-  generateCode();
-  displayCode();
-  event.preventDefault();
+  handler(event);
 });
 
 document.addEventListener("code-refreshed", (event) => {
   console.log("received code-refreshed event", event);
   if (!event.detail || !event.detail.qrcode) return;
+  displayCode(event);
+});
+
+/* PAGES */
+
+const displayCode = (event) => {
+  document.querySelector("#wrapper").className = "displaying-code";
   const { qrcode } = event.detail;
   const qrcodeEl = document.querySelector("#qr-code");
   if (!qrcodeEl) return;
   qrcodeEl.src = qrcode;
   console.log("refreshed the code");
-});
+};
 
-/* PAGES */
-
-const displayCode = () => {
-  document.querySelector("#wrapper").className = "displaying-code";
+const displayPinCode = () => {
+  console.log("TODO: display the pin code");
 };
 
 /* VALIDATION CODES */
@@ -40,4 +45,13 @@ const generateCode = () => {
   });
   console.log("firing code-refreshed event");
   document.dispatchEvent(event);
+};
+
+const printCode = () => {
+  window.print();
+};
+
+const emailCode = () => {
+  location.href =
+    "mailto:sick-person@example.com?subject=QR Code à flasher sur votre application mobile StopCovid";
 };
